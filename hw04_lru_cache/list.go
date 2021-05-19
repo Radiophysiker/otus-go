6a393cb601189ee9e75a9d31ea4b42aaf9e7bba6
radiophysiker <bolshakovdmitry@bk.ru>
2021-05-19 22:15:52 +0300

    git commit -m "add list"

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,76 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	Last   *ListItem
+	First  *ListItem
+	Length int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.Length
+}
+
+func (l *list) Front() *ListItem {
+	return l.First
+}
+
+func (l *list) Back() *ListItem {
+	return l.Last
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := ListItem{
+		Next:  l.First,
+		Value: v,
+	}
+	if l.Last == nil {
+		l.Last = &item
+	}
+	if l.First != nil {
+		l.First.Prev = &item
+	}
+	l.First = &item
+	l.Length++
+	return &item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := ListItem{
+		Prev:  l.Last,
+		Value: v,
+	}
+	if l.Last != nil {
+		l.Last.Next = &item
+	}
+	if l.First == nil {
+		l.First = &item
+	}
+	l.Last = &item
+	l.Length++
+	return &item
+}
+
+func (l *list) Remove(t *ListItem) {
+	if l.First != t {
+		t.Prev.Next = t.Next
+	} else {
+		l.First = t.Next
+	}
+	if l.Last != t {
+		t.Next.Prev = t.Prev
+	} else {
+		l.Last = t.Prev
+	}
+	t.Next = nil
+	t.Prev = nil
+	l.Length--
+}
+
+func (l *list) MoveToFront(t *ListItem) {
+	l.Remove(t)
+	l.PushFront(t.Value)
 }
